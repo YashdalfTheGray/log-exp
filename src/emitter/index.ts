a1de4b67be2397baf106d832f72b9f7967b071dc
Yash Kulshrestha <kulshres@amazon.com>
2023-05-30 21:41:16 -0700

    added the region of where the log group is to the validation

--- src/emitter/index.ts
+++ src/emitter/index.ts
@@ -11,6 +11,7 @@ const {
   AWS_SESSION_TOKEN,
   AWS_REGION,
   AWS_LOGGROUP_NAME,
+  AWS_LOGGROUP_REGION,
   EMIT_INTERVAL_IN_MINUTES,
 } = process.env;
 
@@ -18,7 +19,8 @@ checkVars(
   { name: 'AWS_ACCESS_KEY_ID', value: AWS_ACCESS_KEY_ID },
   { name: 'AWS_SECRET_ACCESS_KEY', value: AWS_SECRET_ACCESS_KEY },
   { name: 'AWS_REGION', value: AWS_REGION },
-  { name: 'AWS_LOGGROUP_NAME', value: AWS_LOGGROUP_NAME }
+  { name: 'AWS_LOGGROUP_NAME', value: AWS_LOGGROUP_NAME },
+  { name: 'AWS_LOGGROUP_REGION', value: AWS_LOGGROUP_REGION }
 );
 
 const emitIntervalInMinutes =
