139fc2dae8dc04b6cc4115d17f0214e8600061ec
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-07-02 11:06:07 -0700

    setting up to generate random json payload

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,12 @@
+import { faker } from '@faker-js/faker';
+
+const FIVE_MINUTES_IN_MS = 5 * 60 * 1000;
+
+const testNames = faker.helpers.uniqueArray(
+  () => faker.lorem.slug({ min: 3, max: 6 }),
+  20
+);
+
 export function checkVars(
   ...varsToCheck: { name: string; value: string | undefined }[]
 ) {
@@ -16,4 +25,14 @@ export function checkVars(
   }
 }
 
-export function generateRandomJson() {}
+export function generateRandomJsonPayload() {
+  const status = '';
+  return {
+    name: faker.helpers.arrayElement(testNames),
+    status,
+    startedTime: Date.now() - FIVE_MINUTES_IN_MS,
+    endedTime: Date.now(),
+    platform: '',
+    error: status ? 'something went wrong' : null,
+  };
+}
