6969fb60ca3aae0f9b8b580d60c347e52d0cfba6
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-07-04 14:43:30 -0700

    actually fixed the shadow mode

--- src/index.ts
+++ src/index.ts
@@ -27,23 +27,23 @@ if (SHADOW === 'true') {
 
   setTimeout(() => {
     clearInterval(interval);
-  }, emitIntervalInMs * 5);
-  console.log('Finished out printing stuff, existing with 0');
-  process.exit(0);
+    console.log('Finished out printing stuff, existing with 0');
+    process.exit(0);
+  }, emitIntervalInMs * 6);
+} else {
+  checkVars(
+    { name: 'AWS_ACCESS_KEY_ID', value: AWS_ACCESS_KEY_ID },
+    { name: 'AWS_SECRET_ACCESS_KEY', value: AWS_SECRET_ACCESS_KEY },
+    { name: 'AWS_REGION', value: AWS_REGION },
+    { name: 'AWS_LOGGROUP_NAME', value: AWS_LOGGROUP_NAME },
+    { name: 'AWS_LOGGROUP_REGION', value: AWS_LOGGROUP_REGION },
+    { name: 'AWS_CUSTOM_METRIC_NAME', value: AWS_CUSTOM_METRIC_NAME },
+    { name: 'AWS_CUSTOM_METRIC_REGION', value: AWS_CUSTOM_METRIC_REGION }
+  );
+
+  (async () => {
+    const ec2Client = getEc2Client();
+    const allRegions = await getAllAvailableRegions(ec2Client);
+    console.log(allRegions);
+  })();
 }
-
-checkVars(
-  { name: 'AWS_ACCESS_KEY_ID', value: AWS_ACCESS_KEY_ID },
-  { name: 'AWS_SECRET_ACCESS_KEY', value: AWS_SECRET_ACCESS_KEY },
-  { name: 'AWS_REGION', value: AWS_REGION },
-  { name: 'AWS_LOGGROUP_NAME', value: AWS_LOGGROUP_NAME },
-  { name: 'AWS_LOGGROUP_REGION', value: AWS_LOGGROUP_REGION },
-  { name: 'AWS_CUSTOM_METRIC_NAME', value: AWS_CUSTOM_METRIC_NAME },
-  { name: 'AWS_CUSTOM_METRIC_REGION', value: AWS_CUSTOM_METRIC_REGION }
-);
-
-(async () => {
-  const ec2Client = getEc2Client();
-  const allRegions = await getAllAvailableRegions(ec2Client);
-  console.log(allRegions);
-})();
