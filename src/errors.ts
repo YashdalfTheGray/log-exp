f1abc5ba6008e261007b120d930254cae4428bc1
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2023-07-08 11:05:54 -0700

    added a custom error that reports ingestion errors

---
+++ src/errors.ts
@@ -0,0 +1,13 @@
+import { PutLogEventsCommandOutput } from '@aws-sdk/client-cloudwatch-logs';
+
+export class LogEventIngestionError extends Error {
+  constructor(
+    private rejectedLogEventsInfo: PutLogEventsCommandOutput['rejectedLogEventsInfo']
+  ) {
+    const message = `Got log rejection errors - ${JSON.stringify(
+      rejectedLogEventsInfo
+    )}`;
+    super(message);
+    this.name = 'LogEventIngestionError';
+  }
+}
